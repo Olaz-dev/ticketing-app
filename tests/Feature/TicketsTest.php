baa7ac90c56db85606474937773954247a69beff
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-24 03:46:04 +0300

    fix test_tickets_table_empty test in TicketsTest

--- tests/Feature/TicketsTest.php
+++ tests/Feature/TicketsTest.php
@@ -2,13 +2,21 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class TicketsTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_tickets_table_empty()
     {
-        $response = $this->get('tickets');
+        $user = User::factory()->admin()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/tickets');
 
         $response->assertStatus(200);
         $response->assertSee(__('No Tickets Found'));
