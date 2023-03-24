86132bbb13b7ec60725eed9cc370f3d33ed0c644
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-24 03:27:44 +0300

    add after to keep timestamps at last

--- database/migrations/2023_02_08_095945_add_role_as_to_users_table.php
+++ database/migrations/2023_02_08_095945_add_role_as_to_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role_as')->default('1');
+            $table->string('role_as')->default('1')->after('password');
         });
     }
 
