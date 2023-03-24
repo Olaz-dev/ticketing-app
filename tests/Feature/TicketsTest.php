ae214e6d4ab9585ca6c2012defda4979c1d76661
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-24 03:46:38 +0300

    add admins_can_access_tickets to TicketsTest

--- tests/Feature/TicketsTest.php
+++ tests/Feature/TicketsTest.php
@@ -22,14 +22,14 @@ class TicketsTest extends TestCase
         $response->assertSee(__('No Tickets Found'));
     }
 
-    // public function test_tickets_page_contains_tickets()
-    // {
-    //     Ticket::create([
-    //         ''
-    //     ]);
-    //     $response = $this->get('/tickets');
-    //     $response->assertStatus(200);
-    //     $response->assertSee(__("No Tickets Found"));
+    public function test_admins_can_access_tickets()
+    {
+        $user = User::factory()->admin()->create();
 
-    // }
+        $response = $this
+            ->actingAs($user)
+            ->get('/tickets');
+
+        $response->assertStatus(200);
+    }
 }
