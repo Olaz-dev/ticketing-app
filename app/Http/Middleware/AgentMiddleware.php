64cd360f5dc85318e907f639bf5fe9b7cd92dff1
Mehmet Nurullah Sağlam <nurullahsaglam@cuzdan.com>
2023-03-24 03:47:25 +0300

    shorter syntax

--- app/Http/Middleware/AgentMiddleware.php
+++ app/Http/Middleware/AgentMiddleware.php
@@ -8,12 +8,10 @@ use Illuminate\Support\Facades\Auth;
 
 class AgentMiddleware
 {
-  
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::user()->role_as == 3) {
-            return $next($request);
-        }
-        abort(403);
+        abort_if(Auth::user()->role_as != 3, 403);
+
+        return $next($request);
     }
 }
